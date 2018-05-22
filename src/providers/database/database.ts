5e87762d3d72a7103c53446aed28a7ebe3bc6564
Gilson <gilsonpolito@gmail.com>
2018-05-22 14:24:51 -0300

    Criação do restante do banco de dados

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -36,7 +36,12 @@ export class DatabaseProvider {
     console.log('createTables');
     db.sqlBatch([
       ['CREATE TABLE IF NOT EXISTS login(email TEXT PRIMARY KEY, password TEXT, perfil TEXT)'],
-      ['CREATE TABLE IF NOT EXISTS instituicao(login_email TEXT PRIMARY KEY, nome TEXT, foto TEXT, FOREIGN KEY(login_email) REFERENCES login(email))']
+      ['CREATE TABLE IF NOT EXISTS instituicao(login_email TEXT PRIMARY KEY, nome TEXT, foto TEXT, FOREIGN KEY(login_email) REFERENCES login(email))'],
+      ['CREATE TABLE IF NOT EXISTS discplina(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, cargaHoraria INTEGER NOT NULL)'],
+      ['CREATE TABLE IF NOT EXISTS professor(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, dataAdmissao NUMERIC NOT NULL)'],
+      ['CREATE TABLE IF NOT EXISTS aluno(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nome TEXT NOT NULL, dataNascimento NUMERIC NOT NULL)'],
+      ['CREATE TABLE IF NOT EXISTS turma(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, disciplina_id INTEGER NOT NULL, professor_id INTEGER NOT NULL, FOREIGN KEY(disciplina_id) REFERENCES disciplina(id), FOREIGN KEY(professor_id)  REFERENCES professor(id))'],
+      ['CREATE TABLE IF NOT EXISTS vinculo(turma_id INTEGER NOT NULL, aluno_id INTEGER NOT NULL PRIMARY KEY(turma_id, aluno_id))']
     ])
     .then(() => console.log('Tabelas criadas'))
     .catch(e => console.error('Erro ao criar as tabelas', e));
